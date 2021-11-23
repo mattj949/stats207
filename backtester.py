41a35f44879ecf657cff07c4f05b80ba575d0735
Matthew Johnson <matthewjohnson@Matthews-MacBook-Pro.local>
2021-11-22 21:10:44 -0800

    backtester working, ARIMA... I have no idea

--- backtester.py
+++ backtester.py
@@ -11,10 +11,10 @@ import numpy as np
 # price_series[i] should be the actual price on day i
 def backtest(forecast_for_each_day, price_series):
     
-    portfolio_value = 1
-    long_only_value = 1
-    list_of_portfolio_values = []
-    list_of_long_only_values = []
+    strategy_portfolio_value = 1
+    long_only_portfolio_value = 1
+    list_strategy_portfolio_values = []
+    list_long_only_portfolio_values = []
     
     # Scale our holding size by the inverse of the asset's price volatility
     pd_series = pd.Series(price_series)
@@ -29,7 +29,7 @@ def backtest(forecast_for_each_day, price_series):
     for i in range(1, len(price_series)):
         # Look back a day and see whether we went long or short the asset
         
-        # If we forecasted a higher price for today than the price yesterday, we go long:
+        # If we forecasted a higher price than yesterday's price, we should have bought the asset.
         if forecast_for_each_day[i] >= price_series[i-1]: 
             long_short = 1
         else:
@@ -37,26 +37,28 @@ def backtest(forecast_for_each_day, price_series):
         
         list_of_weights += [long_short]
 
-        print("Predicted price change: ")
-        print("   " + str(forecast_for_each_day[i] - price_series[i-1]))
-        print("Actual price change:")
-        print("   " + str(price_series[i] - price_series[i-1]))
+        # print("Predicted price change: ")
+        # print("   " + str(forecast_for_each_day[i] - price_series[i-1]))
+        # print("Actual price change:")
+        # print("   " + str(price_series[i] - price_series[i-1]))
 
 
 
         asset_return = ((price_series[i] - price_series[i-1]) / price_series[i-1])
-        holding_return = long_short * scaling * asset_return
         
-        portfolio_value = portfolio_value * (1+holding_return)
-        long_only_value = long_only_value * (1+asset_return)
+        scaled_strategy_return = long_short * scaling * asset_return
+        scaled_long_only_return = scaling * asset_return
         
-        list_of_portfolio_values += [portfolio_value]
-        list_of_long_only_values += [long_only_value]
+        strategy_portfolio_value = strategy_portfolio_value * (1+scaled_strategy_return)
+        long_only_portfolio_value = long_only_portfolio_value * (1+scaled_long_only_return)
+        
+        list_strategy_portfolio_values += [strategy_portfolio_value]
+        list_long_only_portfolio_values += [long_only_portfolio_value]
         
 
     plt.figure(figsize=(18,9))
-    plt.plot(list_of_portfolio_values)
-    plt.plot(list_of_long_only_values)
+    plt.plot(list_strategy_portfolio_values)
+    plt.plot(list_long_only_portfolio_values)
     plt.title("Backtest vs. long only")
     plt.legend(["Strategy return", "Long-Only Return"])
     plt.show()
@@ -66,4 +68,5 @@ def backtest(forecast_for_each_day, price_series):
     plt.figure(figsize=(18,9))
     plt.plot(np.asarray(forecast_for_each_day) - np.asarray(price_series))
     plt.plot(np.asarray(list_of_weights) / 5)
+    plt.legend(["Forecast - actual", "Long or Short"])
     plt.show()
